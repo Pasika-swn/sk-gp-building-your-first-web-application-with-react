051c227383e19846d7542785ef632a8bc283f51a
siriwatknp <siriwatkunaporn@gmail.com>
2023-06-28 07:45:13 +0700

    move inline-style to css

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
 import "@picocss/pico";
+import "./App.css";
 
 function App() {
   return (
-    <main className="container" style={{ marginBlock: "1rem" }}>
-      <h1 style={{ marginBottom: "1rem" }}>Note App</h1>
-      <div
-        style={{
-          display: "flex",
-          gap: "1rem",
-          overflow: "auto",
-          margin: "-1rem",
-          padding: "1rem",
-        }}
-      >
-        <article
-          style={{
-            margin: 0,
-            borderRadius: "8px",
-            border: "1px solid rgba(255 255 255 / 0.6)",
-            padding: "1rem",
-          }}
-        >
-          <div style={{ fontSize: "1.125rem", fontWeight: "bold" }}>
-            My first note
-          </div>
+    <main className="container">
+      <h1 className="app-title">Note App</h1>
+      <div className="note-list">
+        <article className="note-item">
+          <div className="note-title">My first note</div>
         </article>
 
-        <article
-          style={{
-            margin: 0,
-            borderRadius: "8px",
-            border: "1px solid rgba(255 255 255 / 0.6)",
-            padding: "1rem",
-          }}
-        >
-          <div style={{ fontSize: "1.125rem", fontWeight: "bold" }}>
-            My first note
-          </div>
+        <article className="note-item">
+          <div className="note-title">My second note</div>
         </article>
       </div>
 
