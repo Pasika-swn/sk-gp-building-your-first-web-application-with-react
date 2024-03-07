54f0c2faa570552a174e8d741b7390c7e1e1cd8f
Pasika Sriwanna <pasika.swn@gmail.com>
2024-03-07 19:10:25 +0700

    edit note

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import "@picocss/pico";
 import "./App.css";
 
 function App() {
-  const [noteData, setNoteData] = useState({title:"", content:""})
+  const [noteData, setNoteData] = useState({ title: "", content: "" });
 
   const [notes, setNotes] = useState([]);
-
 
   return (
     <main className="container">
@@ -16,9 +15,14 @@ function App() {
         {notes.map((note, index) => (
           <article key={index} className="note-item">
             <div>{note.title}</div>
-            <button onClick={()=>{
-              setNoteData(note)
-            }} className="note-edit-button">✍️</button>
+            <button
+              onClick={() => {
+                setNoteData(note);
+              }}
+              className="note-edit-button"
+            >
+              ✍️
+            </button>
           </article>
         ))}
       </div>
@@ -33,7 +37,9 @@ function App() {
             placeholder="The note's title"
             required
             value={noteData.title}
-            onChange={(e) => setNoteData({...noteData, title:e.target.value})}
+            onChange={(e) =>
+              setNoteData({ ...noteData, title: e.target.value })
+            }
           />
         </label>
 
@@ -46,16 +52,27 @@ function App() {
             placeholder="The note's content"
             required
             value={noteData.content}
-            onChange={(e) => setNoteData({...noteData, content:e.target.value})}
+            onChange={(e) =>
+              setNoteData({ ...noteData, content: e.target.value })
+            }
           />
           <button
             type="button"
             onClick={() => {
               //save the title and content to note
-              setNotes([...notes, noteData]);
-              // setTitle("");
-              // setContent("");
-              setNoteData({title:'', content:""})
+              if (noteData.id) {
+                //save to the existing note
+                setNotes(notes.map((item)=>{
+                  if(item.id === noteData.id) {
+                    return noteData
+                  }
+                  return item //return ของเก่า
+                }))
+              } else {
+                //add new note to the list
+                setNotes([...notes, {...noteData, id: Date.now() }]);
+              }
+              setNoteData({ title: "", content: "" });
             }}
           >
             Submit
